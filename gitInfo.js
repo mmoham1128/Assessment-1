55042949cf488f96eb043943bbb57c93bf419af9
mohamed mohamed <mohamedmohamed@mohameds-MacBook-Air.local>
2022-06-26 23:09:55 -0500

    assessment

---
+++ gitInfo.js
@@ -0,0 +1,90 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+// let gitDefinition = "Git is a software that was released 17 years ago that lets you know what changes were made in the files you were working on during different times. You can always go back to certain saved points and start from there"
+// console.log(gitDefinition)
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+// let gitHubDefinition = "Where you can upload your changes on files you are working on and share them with others."
+// console.log(gitHubDefinition)
+
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+// let gitInitDefinition = "initialize a local repository in the folder you're working on to Github"
+// console.log(gitInitDefinition)
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+// let gitCloneDefinition = "Choosing an exisiting repository and making of copy of it."
+// console.log(gitCloneDefinition)
+
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+// let gitStatusDefinition = "You can see the status of your files, what changes have been made and which files git isn't tracking."
+// console.log(gitStatusDefinition)
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+// let gitAddDefinition = "This adds any changes to the diretory and tells git to include those changes."
+// console.log(gitAddDefinition)
+
+// let gitAddCode = (' git add .')
+
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+// let gitCommitDefinition = "A screenshot of your repository at a certain point when you were working on your file. "
+// console.log(gitCommitDefinition)
+
+// let gitCommitCode = ('git commit -m "initial commit"')
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+// let gitPushDefinition = "Uploads local repository content to a remote location."
+// console.log(gitPushDefinition)
---
+++ sdlc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design 
+// Implementation
+// Testing
+// Maintain 
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+// const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+// const analysisOfRequirements = "Gathering maximum information from the client requirements for the product"
+// const design = "The program developer scrutinizes whether the prepared software suffices all the requirements of the end-user"
+// const implementation = "It means translating the design to a computer-legible language."
+// const testing = "The testing team tests the functionality of the entire system"
+// const maintain = "The developed product is taken care of here to fix any issues that mauy arise or any updates needed."
